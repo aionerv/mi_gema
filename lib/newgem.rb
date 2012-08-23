b765677bce40bac9f2c3eb20a4ed024bdb41c85d
Giovanni Cervera <aionerv@hotmail.com>
2012-08-23 17:27:21 -0500

    First commit

---
+++ lib/newgem.rb
@@ -0,0 +1,12 @@
+require "newgem/version"
+
+module Newgem
+  class GenGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+
+    def generate_holamundo  
+      copy_file "hola_mundo.css", "app/assets/stylesheets/hola_mundo.css"  
+    end
+
+  end
+end
