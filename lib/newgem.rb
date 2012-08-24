044f8d0b68c736f05e3562c26097ab5cf5d7d72d
Giovanni Cervera <aionerv@hotmail.com>
2012-08-24 14:33:12 -0500

    Changed newgem.rb

--- lib/newgem.rb
+++ lib/newgem.rb
@@ -6,9 +6,8 @@ module Newgem
   class GenGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
-    def holamundo  
-      copy_file "hola_mundo.css", "app/assets/stylesheets/hola_mundo.css"  
+    def holamundo
+      template "hola_mundo.css", "app/assets/stylesheets/hola_mundo.css"  
     end
-
   end
 end
--- lib/newgem/railtie.rb
+++ lib/newgem/railtie.rb
@@ -1,5 +1,10 @@
 # lib/newgem/railtie.rb
 module NewGem
   class Railtie < Rails::Railtie
+    def generators(&blk)
+      @generators ||= []
+      @generators << blk if blk
+      @generators
+    end
   end
 end
