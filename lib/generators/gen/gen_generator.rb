9605fe7137e91dc66603d65c35af11e9b090e310
Giovanni Cervera <aionerv@hotmail.com>
2012-08-24 17:16:49 -0500

    Deleted folder rails

---
+++ lib/generators/gen/gen_generator.rb
@@ -0,0 +1,10 @@
+require 'rails/generators/active_record'
+
+class GenGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+
+  def generate_holamundo
+    copy_file "hola_mundo.css", "app/assets/stylesheets/hola_mundo.css"
+  end
+
+end
